69ddf4062db943fa94d288e617e2015d6785d200
rrasmuss07 <rrasmuss@ualberta.ca>
2025-10-27 10:15:39 -0600

    make types more robust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let bytes_to_compress = &mut [
+    let bytes_to_compress: &mut [u8; 24] = &mut [
     0x03, 0x74, 0x04, 0x04, 0x04, 0x35, 0x35, 0x64,
     0x64, 0x64, 0x64, 0x00, 0x00, 0x00, 0x00, 0x00,
     0x56, 0x45, 0x56, 0x56, 0x56, 0x09, 0x09, 0x09
@@ -9,7 +9,7 @@ fn main() {
     println!("Compressed size {}", compressed_bytes);
 
     println!("Try large data set:");
-    let mut large_array = Vec::new();
+    let mut large_array: Vec<u8> = Vec::new();
     for _ in 0..100 {
         large_array.push(0x00)
     }
@@ -37,7 +37,7 @@ fn main() {
 
 fn compress_bytes(data_ptr: &mut [u8], data_size: usize) -> usize {
     let mut byte_count: usize = 1;
-    let mut i = 0;
+    let mut i: usize = 0;
     let mut new_vec: Vec<u8> = Vec::new();
     while i < data_size - 1 {
         if data_ptr[i] == data_ptr[i+1] {
@@ -53,6 +53,6 @@ fn compress_bytes(data_ptr: &mut [u8], data_size: usize) -> usize {
     // handle last run
     new_vec.push(byte_count as u8);
     new_vec.push(data_ptr[data_size - 1]);
-    let data_ptr = &new_vec;
+    let data_ptr: &Vec<u8> = &new_vec;
     data_ptr.len()
 }
